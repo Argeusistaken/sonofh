048ea8a404e35c7b1215d4a09a3f053fba3dd0a5
ArincYanar <arincyanar1@gmail.com>
2026-06-25 18:13:04 +0300

    dsadasd
    dsadsadaa

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
     [SerializeField] private float speed = 5f;
     private Rigidbody2D rb2d;
     private Vector2 moveInput;
@@ -22,21 +24,32 @@ public class PlayerMovement : MonoBehaviour
     
         moveInput = value.Get<Vector2>();
 
-        //if ( Oyuncu hareket etmeyi bıraktığında )
-        //{
-        //    animator.SetBool("isWalking", false);
-        //    animator.SetFloat("LastInputX", moveInput.x);
-        //    animator.SetFloat("LastInputY", moveInput.y);
-        //}
+       
 
         animator.SetFloat("InputX", moveInput.x);
         animator.SetFloat("InputY", moveInput.y);
-        
+        if (Keyboard.current.aKey.wasPressedThisFrame)
+        {
+            rb2d.transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (Keyboard.current.dKey.wasPressedThisFrame)
+        {
+            rb2d.transform.eulerAngles = new Vector3(0, 0, 0);
+        }
         // Bir de oyuncu sola doğru hareket ettiği zaman karakterin ters tarafa dönmesi lazım
     }
 
     void FixedUpdate()
     {
         rb2d.linearVelocity = moveInput * speed;
+        if (rb2d.linearVelocity == Vector2.zero)
+        {
+
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+            animator.SetBool("isWalking", false);
+            
+        }
+       
     }
 }
